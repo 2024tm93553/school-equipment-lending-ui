87a583743206be54ef3382d791abb1f19fd44aaa
2024tm93553 <2024tm93553@wilp.bits-pilani.ac.in>
2025-11-09 18:37:50 +0530

    Integrate Login and Register component in APp root and add routing logics

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import RegisterComponent from './components/RegisterComponent'
+import LoginComponent from './components/LoginComponent'
+import { isUserLoggedIn } from './services/AuthService'
+
+function App() {
+
+  function AuthenticatedRoute({children}){
+
+    const isAuth = isUserLoggedIn();
+
+    if(isAuth) {
+      return children;
+    }
+    return <Navigate to="/" />
+
+  }
+
+  return (
+    <>
+    <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+              <Route path='/' element = { <LoginComponent /> }></Route>
+               <Route path='/register' element = { <RegisterComponent />}></Route>
+               <Route path='/login' element = { <LoginComponent /> }></Route>
+          </Routes>
+        <FooterComponent />
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
