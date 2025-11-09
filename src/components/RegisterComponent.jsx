e3130e60df627a90666bf303520bcae27b169d17
2024tm93553 <2024tm93553@wilp.bits-pilani.ac.in>
2025-11-09 18:37:00 +0530

    Implement Login and Registration components

---
+++ src/components/RegisterComponent.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react'
+import { registerAPICall } from '../services/AuthService'
+
+const RegisterComponent = () => {
+
+    const [fullName, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [role, setRole] = useState('STUDENT')
+
+
+    function handleRegistrationForm(e){
+
+        e.preventDefault();
+
+        const register = {fullName, username, email, password, role}
+
+        console.log(register);
+
+        registerAPICall(register).then((response) => {
+            console.log(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+  return (
+    <div className='container'>
+        <br /> <br />
+        <div className='row'>
+            <div className='col-md-6 offset-md-3'>
+                <div className='card'>
+                    <div className='card-header'>
+                        <h2 className='text-center'> User Registration Form </h2>
+                    </div>
+
+                    <div className='card-body'>
+                        <form>
+                            <div className='row mb-3'>
+                                <label className='col-md-3 control-label'> Name </label>
+                                <div className='col-md-9'>
+                                    <input
+                                        type='text'
+                                        name='name'
+                                        className='form-control'
+                                        placeholder='Enter name'
+                                        value={fullName}
+                                        onChange={ (e) => setName(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+                            </div>
+
+                            <div className='row mb-3'>
+                                <label className='col-md-3 control-label'> Username </label>
+                                <div className='col-md-9'>
+                                    <input
+                                        type='text'
+                                        name='username'
+                                        className='form-control'
+                                        placeholder='Enter username'
+                                        value={username}
+                                        onChange={ (e) => setUsername(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+                            </div>
+
+
+                            <div className='row mb-3'>
+                                <label className='col-md-3 control-label'> Email </label>
+                                <div className='col-md-9'>
+                                    <input
+                                        type='text'
+                                        name='email'
+                                        className='form-control'
+                                        placeholder='Enter email address'
+                                        value={email}
+                                        onChange={ (e) => setEmail(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+                            </div>
+
+                            <div className='row mb-3'>
+                                <label className='col-md-3 control-label'> Password </label>
+                                <div className='col-md-9'>
+                                    <input
+                                        type='password'
+                                        name='password'
+                                        className='form-control'
+                                        placeholder='Enter password'
+                                        value={password}
+                                        onChange={ (e) => setPassword(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+                            </div>
+
+                            <div className='row mb-3'>
+                                <label className='col-md-3 control-label'> Role </label>
+                                <div className='col-md-9'>
+                                    <select
+                                        className='form-control'
+                                        value={role}
+                                        onChange={ (e) => setRole(e.target.value)}
+                                    >
+                                        <option value='STUDENT'>STUDENT</option>
+                                        <option value='TEACHER'>TEACHER</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className='form-group mb-3'>
+                                <button className='btn btn-primary' onClick={ (e) => handleRegistrationForm(e)}>Submit</button>
+
+                            </div>
+                        </form>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default RegisterComponent
