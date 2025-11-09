3698b847ae4cf3f748015a9466bf17f9187ffe17
2024tm93553 <2024tm93553@wilp.bits-pilani.ac.in>
2025-11-09 19:04:48 +0530

    Add proper routings to Equipment APIS

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import RegisterComponent from './components/RegisterComponent'
 import LoginComponent from './components/LoginComponent'
 import { isUserLoggedIn } from './services/AuthService'
+import EquipmentListComponent from "../srcNew/components/EquipmentListComponent";
+import EquipmentFormComponent from "../srcNew/components/EquipmentFormComponent";
 
 function App() {
 
@@ -25,7 +27,27 @@ function App() {
         <HeaderComponent />
           <Routes>
               <Route path='/' element = { <LoginComponent /> }></Route>
-               <Route path='/register' element = { <RegisterComponent />}></Route>
+
+              <Route path='/equipment' element = {
+                  <AuthenticatedRoute>
+                      <EquipmentListComponent />
+                  </AuthenticatedRoute>
+              }></Route>
+
+              <Route path='/add-equipment' element = {
+                  <AuthenticatedRoute>
+                      <EquipmentFormComponent />
+                  </AuthenticatedRoute>
+              }></Route>
+
+              <Route path='/edit-equipment/:id' element = {
+                  <AuthenticatedRoute>
+                      <EquipmentFormComponent />
+                  </AuthenticatedRoute>
+              }></Route>
+
+
+              <Route path='/register' element = { <RegisterComponent />}></Route>
                <Route path='/login' element = { <LoginComponent /> }></Route>
           </Routes>
         <FooterComponent />
